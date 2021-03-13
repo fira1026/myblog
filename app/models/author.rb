e2eae9a516bd3940dc9da8bdd1ddc961d1f25ad1
fira1026 <fira1026@gmail.com>
2021-03-14 00:54:22 +0800

    Add Author validations

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,16 @@
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
 class Author < ApplicationRecord
   has_many :posts
-  validates :name, presence: true
+
+  validates :name, presence: true, 
+    uniqueness: { case_sensitive: false }, 
+    length: { minimum: 3, maximum: 25 }
+
+  validates :email, presence: true, 
+    length: { maximum: 105 },
+    uniqueness: { case_sensitive: false }, 
+    format: { with: VALID_EMAIL_REGEX }
+    before_save { self.email = email.downcase }
+    
 end
