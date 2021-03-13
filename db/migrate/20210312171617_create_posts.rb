e3fb371f25d6eb5f04607582d87b1d1aef05a2a7
fira1026 <fira1026@gmail.com>
2021-03-14 06:08:36 +0800

    Remove invalid migrations files

--- db/migrate/20210312171617_create_posts.rb
+++ db/migrate/20210312171617_create_posts.rb
@@ -2,8 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.string :description
-      t.text :body
+      t.string :content
       t.integer :author_id
 
       t.timestamps
